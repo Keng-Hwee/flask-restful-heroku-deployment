05ba9bdb92e83bda52464ba2b0750d3c77c8ce52
Keng Hwee <kenghwee.t@gmail.com>
2019-05-23 22:36:24 +0800

    Added postgresSQL environment and psycopsg2

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask, request, jsonify
 from flask_restful import Api
 from flask_jwt_extended import JWTManager
@@ -7,9 +9,9 @@ from resources.user import UserRegister
 from resources.item import Item, ItemList
 from resources.store import Store, StoreList
 
-
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL',
+                                                       'sqlite:///data.db')  # second param is the default value
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['PROPAGATE_EXCEPTIONS'] = True
 api = Api(app)
@@ -17,6 +19,7 @@ api = Api(app)
 # Setup flask-jwt-extended
 app.config['JWT_SECRET_KEY'] = 'kh'
 jwt = JWTManager(app)  # creates a new endpoint: /auth
+
 
 # Provide a method to create access tokens. The create_access_token()
 # function is used to actually generate the token, and you can return
@@ -44,5 +47,6 @@ api.add_resource(UserRegister, '/register')
 
 if __name__ == '__main__':
     from db import db
+
     db.init_app(app)
     app.run(port=5000, debug=True)
