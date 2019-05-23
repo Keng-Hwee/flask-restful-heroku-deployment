ac997548d3d6178180f06379a0fcd2658d428f37
Keng Hwee <kenghwee.t@gmail.com>
2019-05-23 22:13:58 +0800

    code checking for circular imports

--- app.py
+++ app.py
@@ -7,17 +7,16 @@ from resources.user import UserRegister
 from resources.item import Item, ItemList
 from resources.store import Store, StoreList
 
+
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['PROPAGATE_EXCEPTIONS'] = True
 api = Api(app)
 
-
 # Setup flask-jwt-extended
 app.config['JWT_SECRET_KEY'] = 'kh'
 jwt = JWTManager(app)  # creates a new endpoint: /auth
-
 
 # Provide a method to create access tokens. The create_access_token()
 # function is used to actually generate the token, and you can return
@@ -45,6 +44,5 @@ api.add_resource(UserRegister, '/register')
 
 if __name__ == '__main__':
     from db import db
-
     db.init_app(app)
     app.run(port=5000, debug=True)
